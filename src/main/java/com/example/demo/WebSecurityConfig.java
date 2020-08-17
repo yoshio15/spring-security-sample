156ff86f0c6c05341acf2915d2ddca7b50eef92b
YoshioKondo <yoshio15310@gmail.com>
2020-08-17 23:53:08 +0900

    add spring security

---
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    //ここに設定情報を定義していく
+}
