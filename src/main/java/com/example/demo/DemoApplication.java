764fa3c92459ea93e55656f999314895c500ba83
YoshioKondo <yoshio15310@gmail.com>
2020-08-17 23:28:47 +0900

    hello world まで

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
